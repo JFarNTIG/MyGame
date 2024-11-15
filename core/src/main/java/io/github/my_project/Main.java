21f9728877eedd6b4d9aff6622f7f7024a11a2a2
Jacob Lewis Farnsworth <jacoblewis.farnsworth@ntig.se>
2024-11-15 12:22:02 +0100

    Gg 1 Komma igång, ruta, färger

--- core/src/main/java/io/github/my_project/Main.java
+++ core/src/main/java/io/github/my_project/Main.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
@@ -12,23 +14,41 @@ public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
 
+    // Skapa en ShapeRenderer.
+    private ShapeRenderer shapeRenderer;
+
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+
+        // Initiera ShapeRenderer.
+        shapeRenderer = new ShapeRenderer();
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        ScreenUtils.clear(0.0f, 0.0f, 0.0f, 1.0f);
+
         batch.begin();
         batch.draw(image, 140, 210);
         batch.end();
+
+        // Rita en blå rektangel med ShapeRenderer.
+        shapeRenderer.begin(ShapeType.Filled);
+        shapeRenderer.setColor(0, 0, 1, 1);
+        shapeRenderer.rect(100, 500, 100, 200);
+        shapeRenderer.end();
+
+        // Övning: Kan du rita en gul cirkel med ShapeRenderer?
     }
 
     @Override
     public void dispose() {
         batch.dispose();
         image.dispose();
+
+        // Städa upp: se till att köra dispose() på ShapeRenderer
+        shapeRenderer.dispose();
     }
 }
